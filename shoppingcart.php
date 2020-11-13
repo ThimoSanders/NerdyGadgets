137641970e1f281b01904b6212cf38a7045ee679
StijnMeppelink <stijnmeppelink@hotmail.com>
2020-11-13 10:51:00 +0100

    de loop afgemaakt

--- shoppingcart.php
+++ shoppingcart.php
@@ -7,23 +7,18 @@ include __DIR__ . "/header.php";
         <br>
         <h1>winkelwagen</h1>
     </header>
-<?php
+
+<p>
+
+    <?php
 $totalPrice = 0;
 if (!isset($_SESSION['shoppingcart'])) {
     print "<h1>Er zit niks in je winkelwagen</h1>";
 } else {
     //foreach loop for every item in the shopping cart
-    foreach ($_SESSION['shoppingcart'] as $key => $value) {
+    foreach ($_SESSION['shoppingcart'] as $productID => $value) {
         //prints the key, in this case the "StockItemID"
-        print("key: ".$key);
-        //prints the value, in this case the quantity
-        print(" value: ".$value);
-        //prints <br>... duh
-        print("<br>");
-    }
-
-    $productID = 96;
-
+        print("product ID: ".$productID);
     $Connection = mysqli_connect("localhost", "root", "", "nerdygadgets", "3306");
     $Query = "SELECT ImagePath
               FROM stockitemimages 
@@ -42,19 +37,30 @@ if (!isset($_SESSION['shoppingcart'])) {
     mysqli_stmt_execute($Statement);
     $N = mysqli_stmt_get_result($Statement);
     $N = mysqli_fetch_all($N, MYSQLI_ASSOC);
-?>
+    $totalPrice += $N[0]['RecommendedRetailPrice'] * $value;
+    ?>
+
     <div id="ImageFrame"
-         style="background-image: url('Public/StockItemIMG/<?php print $R[0]['ImagePath']; ?>'); background-size: 300px; background-repeat: no-repeat; background-position: center;">
+         style="background-image: url('Public/StockItemIMG/<?php print $R[0]['ImagePath']; ?>'); background-size: 100px; background-repeat: no-repeat; background-position: center;">
     </div>
     <h2><?=$N[0]['StockItemName']?></h2>
-    <h5><?=$N[0]['RecommendedRetailPrice']?></h5>
+    <h5>€<?=$N[0]['RecommendedRetailPrice']?></h5>
 
-    <h3>totaalprijs: <?=$totalPrice?></h3>
+    <h3>totaal: €<?=$N[0]['RecommendedRetailPrice'] * $value?></h3>
 </div>
 <?php
+         print(" Aantal: ".$value);?>
+<br>
+   <?php     //prints <br>... duh
+        print("<br>");
+    }
+
+
 }
+print ("€") .$totalPrice;
 ?>
+</p>
 
-
-
-
+<tr>
+    <td><button id="betalen">Betalen</button></td>
+</tr>
