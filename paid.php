9bb1574113edb471f8a6a4a3bcb743c96f7c0012
Bastiaan7 <74175081+Bastiaan7@users.noreply.github.com>
2020-11-16 13:24:37 +0100

    updated paid.php

--- paid.php
+++ paid.php
@@ -1,1 +1,24 @@
 <?php
+include __DIR__ . "/header.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="Public/CSS/style_betalingspagina.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <div class="z">
+        <label>Bedankt voor uw bestelling</label>
+    </div>
+    <div class="z2"
+         <label>Uw bestelling is geplaatst en wordt momenteel verwerkt. U zal een email ontvangen met de details van de bestelling</label>
+    </div>
+
+</head>
+<body>
+
+
+
+</body>
+</html>
