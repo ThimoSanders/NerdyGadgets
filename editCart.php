38d343688974c950a7f2fddfce873b77ce637f8f
Wouter <woutervandervelde2000@hotmail.com>
2020-11-16 20:17:02 +0100

    added feature to edit shopping cart

---
+++ editCart.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+try {
+    $ItemID = $_GET['ItemID'];
+    $Quantity = $_GET['Quantity'];
+    $Price = $_GET['Price'];
+    $TotalPrice = $_GET['TotalPrice'];
+
+    if (!isset($_SESSION['shoppingcart'])) {
+        throw new Exception("No session");
+    }
+    if (!array_key_exists( $ItemID, $_SESSION['shoppingcart'])) {
+        throw new Exception("ItemID not in shoppingcart");
+    }
+    if ($Quantity < 0 || !is_numeric($Quantity)) {
+        throw new Exception("Invalid quantity");
+    }
+    if ($Quantity == 0) {
+        unset($_SESSION['shoppingcart'][$ItemID]);
+        if (count($_SESSION['shoppingcart']) == 0) {
+            unset($_SESSION['shoppingcart']);
+            $_SESSION['totalPrice'] = 0;
+        }
+    } else {
+        $_SESSION['shoppingcart'][$ItemID] = $Quantity;
+        $_SESSION['totalPrice'] = $Price * $Quantity + $TotalPrice;
+    }
+    echo json_encode(["message" => "success"]);
+} catch (Exception $e) {
+    echo json_encode(["error" => $e->getMessage()]);
+}
