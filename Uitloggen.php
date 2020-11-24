c022feb9cf1f534934565ad35c30da8f9560fdf4
unknown <s1159311@student.windesheim.nl>
2020-11-24 11:19:06 +0100

    uitloggen

--- Uitloggen.php
+++ Uitloggen.php
@@ -1,1 +1,23 @@
 <?php
+include __DIR__ . "/header.php";
+
+unset(($_SESSION)["login"]);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+    <link rel="stylesheet" type="text/css" href="Public/CSS/style_betalingspagina.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <div class="z">
+        <label>U bent uitgelogd</label>
+    </div>
+    <div class="z2">
+        <a href="login.php" class="HrefDecoration"><i style="color:#676EFF;"></i>Als u hierop klikt kunt u weer inloggen.</a>
+    </div>
+
+</head>
+<body>
