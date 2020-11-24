62261fcbf5aed4a5d189d860c8216626b72de301
Wouter <woutervandervelde2000@hotmail.com>
2020-11-24 09:39:39 +0100

    changed script order, popper before bootstrap

--- header.php
+++ header.php
@@ -7,8 +7,8 @@ include "connect.php";
 <head>
     <script src="Public/JS/fontawesome.js" crossorigin="anonymous"></script>
     <script src="Public/JS/jquery.min.js"></script>
-    <script src="Public/JS/bootstrap.min.js"></script>
     <script src="Public/JS/popper.min.js"></script>
+    <script src="Public/JS/bootstrap.min.js"></script>
     <script src="Public/JS/Resizer.js"></script>
     <script src="Public/JS/jquery-3.4.1.js"></script>
     <style>
